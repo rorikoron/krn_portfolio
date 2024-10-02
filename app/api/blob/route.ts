5b8b18e413bd4ac2b3eae6a764c5e44b15de433f
rorikoron <sub.ac.000000001@gmail.com>
2024-10-03 01:52:28 +0800

    fixed lint issue-2

--- app/api/blob/route.ts
+++ app/api/blob/route.ts
@@ -6,8 +6,8 @@ export async function GET() {
         const name_full = file.substring(file.lastIndexOf('\\')+1);
         const name_extract = name_full?.substring(0, name_full.lastIndexOf('.'));
         return({
-            fileName: name_extract,
-            path: `/archive/${name_full}`
+            fileName: name_extract.substring(name_extract.lastIndexOf('\\')+1),
+            path: `${name_full}`
         })
     })
     return Response.json(fileInfo);
